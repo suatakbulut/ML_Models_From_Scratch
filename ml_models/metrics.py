de5006234e5010915073bc4ba23857fb9e09b8be
suatakbulut <akbulut.suat@gmail.com>
2023-10-12 23:15:30 -0400

    created r2_score in metrics.py

--- ml_models/metrics.py
+++ ml_models/metrics.py
@@ -6,3 +6,9 @@ def mean_square_error(y_true, y_pred):
 
 def accuracy(y_true, y_pred):
     return np.sum(y_true==y_pred) / len(y_true)
+
+
+def r2_score(y_true, y_pred):
+    TSS = np.sum( (y_true - y_true.mean())**2 )
+    ESS = np.sum( (y_true - y_pred)**2 )
+    return 1 - (ESS/TSS)
